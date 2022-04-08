1940b5b44c1fcd3c0dbc066fb6a03edbb8b6af4e
jodevo1 <eelolorunvic@gmail.com>
2022-04-08 05:06:14 -0700

    Print all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char sml, caps;
+
+	for (sml = 'a'; sml <= 'z'; sml++)
+		putchar(sml);
+
+	for (caps = 'A'; caps <= 'Z'; caps++)
+		putchar(caps);
+
+	putchar('\n');
+
+	return (0);
+}
